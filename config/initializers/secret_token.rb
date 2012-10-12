384a68897bd942c1c023ba17cc397172a9485b31
paula <caminopaula@gmail.com>
2012-10-12 18:11:30 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Padelotron::Application.config.secret_token = '1fe887b27292c687eec643db566491582f91b9e383f511ec36a002bae62aa71e65fc0286bf2a377cddf25ce2fc3ce689209ca6fc219d6a7c09336c4fcc25cb8b'
